2b0d30ac3912eb2907945edcbbff157fb1dd4ab7
Ashu <ashenshi@gmail.com>
2020-03-09 15:12:20 +0100

    layout folder and hbs file created along with routes on app.js

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 require("./models/db");
 const express = require("express");
 const booksController = require("./controllers/booksController");
+ 
+const path = require("path");
+const exphbs = require("express-handlebars");
+//to send data to database 1
+const bodyParser = require("body-parser");
 const app = express();
+//to send data to database 2
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+//together with the above
+app.use(bodyParser.json())
+
+
+//create a new folder and put hbs files there
+app.set("views", path.join(__dirname, "/views/"));
+app.engine("hbs", exphbs ({extname: "hbs", defaultLayout: "mainLayout", layoutsDir: __dirname + "/views/layouts/"}));
+app.set("view engine", "hbs");
+
 
 app.listen(3000, ()=>{
     console.log("Express server is connected at port: 3000!")
