7875fbadad1225d2b402467353da481fd3f54dc6
Ashu <ashenshi@gmail.com>
2020-03-09 14:52:57 +0100

    controller and views and books forlder with addOrEdith file added

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require("./models/db");
 const express = require("express");
+const booksController = require("./controllers/booksController");
 const app = express();
 
 app.listen(3000, ()=>{
     console.log("Express server is connected at port: 3000!")
 })
+
+
+//localhost:3000/employee  is our route
+app.use("/books", booksController);
--- controllers/booksController.js
+++ controllers/booksController.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const myBooks = mongoose.model("myBooks");
 
-
+//route just to show the form on the addOrEdit in the browser
+router.get("/", (req, res)=>{
+    //this msg ll be printed the employee route is choosen on browser
+    res.render("books/addOrEdit", {viewTitle: "Insert Books"})
+})
 
 
 
