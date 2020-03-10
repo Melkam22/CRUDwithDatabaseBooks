c8431607a11161bd145be0f3c46078a2d450a33c
Ashu <ashenshi@gmail.com>
2020-03-10 12:59:35 +0100

    list route added but post route is not working

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -9,12 +9,61 @@ router.get("/", (req, res)=>{
     res.render("books/addOrEdit", {viewTitle: "Insert Books"})
 })
 
-//push data to database
-router.post("/", (req, res)=>{
+//push data to database, inserted infos showen on terminal below to show them on the dom
+/* router.post("/", (req, res)=>{
   console.log(req.body)  
-})
+}) */
 
+//insert function added in route add to database
+router.post("/", (req,res) => {
+    if(req.body._id == "")
+       insertBooks(req,res)
+    else
+       updateBooks(req, res)
+});
 
+function insertBooks(req,res){
+    let booksList = new myBooks();
+    booksList.authorName = req.body.authorName;
+    booksList.bookTitle = req.body.bookTitle;
+    booksList.publicationYear = req.body.publicationYear;
+    booksList.publishedAt = req.body.publishedAt;
+    booksList.currentLanguage = req.body.currentLanguage;
+    booksList.availability = req.body.availability
+    booksList.save((err, doc)=>{
+        if(!err)
+        res.redirect("books/list");
+        //email validation
+        else 
+            console.log("error in pushing to db occurs" + err)
+    })
+
+    }
+
+//related function
+function updateBooks(req, res){
+    myBooks.findByIdAndUpdate({_id: req.body._id}, req.body, {new: true}, (err,doc)=>{
+        if(!err){
+            res.redirect("books/list")
+         
+            }
+            else 
+            console.log("Eror in recording update", + err)
+        })
+    }
+//route for list
+    router.get("/list", (req,res)=>{
+        myBooks.find((err, docs)=>{
+            if(!err){
+                res.render("books/list", {
+                    list: docs
+                })
+            }
+            else{
+                console.log("Error in retrieving books list" + err)
+            }
+        })
+    })
 
 
 module.exports = router;
